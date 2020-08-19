806e0599914b780c4b155de0f9d56cbf77c89f43
Timothy Saudners <timsaunders88@gmail.com>
2020-08-19 18:08:37 +0100

    tests passing anagram_finder

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,24 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
 }
+
+AnagramFinder.prototype.standardise = function(word){
+    return word.toLowerCase().split('').sort().join('');
+};
+
+AnagramFinder.prototype.isAnagram = function(word1, word2){
+    if (word1 === word2){
+        return false;
+    } else{
+        return this.standardise(word1) === this.standardise(word2);
+    };
+};
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
-}
+    let result = otherWords.filter((word) => {
+      return this.isAnagram(this.word, word);
+    });
+    return result
+};
 
 module.exports = AnagramFinder;
