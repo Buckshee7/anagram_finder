19e678c676a8750b2bf9db06df8f0a3c20226cda
Timothy Saudners <timsaunders88@gmail.com>
2020-08-19 16:31:19 +0100

    pngram_finder tests passing

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,20 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
-PangramFinder.prototype.isPangram = function () {
 
+PangramFinder.prototype.isPangram = function () {
+  phraseArray = this.phrase.split('');
+  //true if every letter in alphabet is in phrase
+  const result = this.alphabet.every((alphabet_letter) => {
+    // condition must be true if letter is in phrase
+    return phraseArray.some((provided_letter) => {
+      // check if individual alphabet letter is in phrase
+      return provided_letter.toLowerCase() === alphabet_letter;
+    });
+  })
+  return result;
 }
 
 module.exports = PangramFinder;
