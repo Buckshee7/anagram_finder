ef4a8d2d6f633d674daadc9f03e070e58741c4d6
Timothy Saudners <timsaunders88@gmail.com>
2020-08-19 17:12:19 +0100

    passing tests for isogram_finder

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,20 @@
 const IsogramFinder = function (word) {
+    this.word = word
+}
 
+IsogramFinder.prototype.moreThanOne = function(item, array){
+    const howMany = array.filter(element => element === item).length;
+    if (howMany > 1){
+        return true;
+    }
+    return false;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
-}
+    const splitWord = this.word.split('');
+    return splitWord.every((letter) => {
+        return !this.moreThanOne(letter, splitWord);
+    });
+};
 
 module.exports = IsogramFinder;
